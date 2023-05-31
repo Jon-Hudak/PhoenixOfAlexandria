ec64e54e584f23f45e9c4d211ff25173ce9f80a6
Jon-Hudak <shigshagor@gmail.com>
2023-05-30 20:47:03 -0400

    fix: configs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig, sharpImageService } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import image from "@astrojs/image";
+
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), image({
     serviceEntryPoint: '@astrojs/image/sharp'
-  })],
+  }), preact()],
   site: "https://www.PLACEHOLDER.com"
 });
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,13 +4,23 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
-			fontFamily:{
+			fontFamily: {
 				serif: ["Cormorant Garamond", "Cormorant", ...defaultTheme.fontFamily.serif]
 			},
-			colors:{
+			colors: {
 				offBlack: "#03090f",
 				secondary: "#EDEDED"
-			}
+			},
+			keyframes: {
+				'move-left': {
+					'from': { opacity:"0" },
+					'to': { transform: 'translateX(0)',opacity:"1" }
+
+				}
+			},
+			animation: {
+				'move-left': 'move-left 0.2s ease-out forwards'
+			},
 		},
 	},
 	plugins: [],
