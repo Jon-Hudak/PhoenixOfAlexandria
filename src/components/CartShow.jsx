cfb77c2c54c653fc42010fb07dbb8d7f75b97cdf
Jon-Hudak <shigshagor@gmail.com>
2023-06-02 21:15:44 -0400

    fix: Minor formatting

--- src/components/CartShow.jsx
+++ src/components/CartShow.jsx
@@ -3,6 +3,7 @@ import { useStore } from '@nanostores/preact';
 
 function CartShow({children}) {
   const $cartQty=useStore(cartQty);
+  // 
   return (
     <button onClick={()=>{isCartOpen.set(!isCartOpen.get())}} class="flex place-content-end ml-auto md:ml-0 mr-4 sm:mr-4 relative">
       {$cartQty>0 && <span class="bg-orange-500 rounded-full h-6 w-6 absolute -top-2 -right-2">{$cartQty}</span>}
