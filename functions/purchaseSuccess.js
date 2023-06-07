ef9bf1872baea4bf4182fab815ef484e07a11bc7
Jon-Hudak <shigshagor@gmail.com>
2023-06-07 03:26:31 -0400

    wip: added import statement

--- functions/purchaseSuccess.js
+++ functions/purchaseSuccess.js
@@ -1,5 +1,6 @@
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
+import sgMail from "@sendgrid/mail"
 
 const environment = process.env.CONTEXT;
 const webhookSecretKey = process.env.STRIPE_WEBHOOK_SECRET
@@ -7,7 +8,7 @@ const webhookSecretKey = process.env.STRIPE_WEBHOOK_SECRET
 
 // const stripeKey = environment !== "production" ? process.env.STRIPE_TEST_KEY : "ADD PRODUCTION KEY";
 const stripeKey = process.env.STRIPE_TEST_KEY;
-const sgMail = require("@sendgrid/mail");
+//const sgMail = require("@sendgrid/mail");
 const stripe = require("stripe")(stripeKey);
 
 
